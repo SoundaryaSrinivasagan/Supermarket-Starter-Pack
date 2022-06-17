4612fe9655b15495c19ba0b7dcbc53767871603b
deelango <deelango96@gmail.com>
2022-06-17 14:25:18 -0400

    Update

--- scripts/checkout_counter.py
+++ scripts/checkout_counter.py
@@ -4,29 +4,30 @@ import sys
 # adding sample_data to the system path
 sys.path.insert(0, '/Desktop/Supermarket-Starter-Pack/sample_data')
 
-'''
- Function:         total_cost_before_tax(trans_num)
- Description:      This function will return the number of items present in any transaction before taxes
- Input Parameters: trans_num : Int
-                               trans_num is the number of the transaction you want to pull from transaction_data
-'''
-def total_cost_before_tax (trans_num):
-  cost = 0
-  for item in transaction_no(trans_num):
-    cost = cost + item_dict[item]
+class checkout_counter:
+      '''
+       Function:         total_cost_before_tax(trans_num)
+       Description:      This function will return the number of items present in any transaction before taxes
+       Input Parameters: trans_num : Int
+                                     trans_num is the number of the transaction you want to pull from transaction_data
+      '''
+      def total_cost_before_tax (trans_num):
+        cost = 0
+        for item in transaction_no(trans_num):
+          cost = cost + item_dict[item]
 
-  return(cost)
+        return(cost)
 
-'''
- Function:         find_total(trans_num)
- Description:      This Function should be able to find the final price of any transaction after Taxes have been implemented
- Input Parameters: trans_num : Int
-                               trans_num is the number of the transaction you want to pull from transaction_data
-'''
-def find_total(trans_num):
-# Note that the taxes are assumed to be 5%, but they can be modified to the actual HST with respect to your country
-  final_price = total_cost_before_tax(trans_num) * 1.05
+      '''
+       Function:         find_total(trans_num)
+       Description:      This Function should be able to find the final price of any transaction after Taxes have been implemented
+       Input Parameters: trans_num : Int
+                                     trans_num is the number of the transaction you want to pull from transaction_data
+      '''
+      def find_total(trans_num):
+      # Note that the taxes are assumed to be 5%, but they can be modified to the actual HST with respect to your country
+        final_price = total_cost_before_tax(trans_num) * 1.05
 
-  return(final_price)
+        return(final_price)
 
 
--- test/test_checkout_counter.py
+++ test/test_checkout_counter.py
@@ -1,5 +1,6 @@
 # importing sys
 import sys
+import unittest
 
 # adding sample_data to the system path
 sys.path.insert(0, '/Desktop/Supermarket-Starter-Pack/sample_data')
@@ -7,8 +8,8 @@ sys.path.insert(0, '/Desktop/Supermarket-Starter-Pack/scripts')
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        self.assertEqual(16, checkout_counter.total_cost_before_tax (9))  # add assertion here
 
 
 if __name__ == '__main__':
-    MyTestCase.main()
+    unittest.main()
